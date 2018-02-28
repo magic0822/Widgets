0049806586d6a75bf1e2736d35814cade0cefe4f
magic0822 <magic.fan.ys@gmail.com>
2018-02-27 23:30:43 -0500

    add class

--- Database.php
+++ Database.php
@@ -6,22 +6,15 @@
  * Time: 7:40 PM
  */
 
-class DB extends SQLite3
-{
-    function __construct()
-    {
-        $this->open('widgets.db');
-    }
+require 'Model.class.php';
 
-}
-
-$db = new DB();
+$db = new Model();
 $create_tale =<<<EOF
 CREATE TABLE IF NOT EXISTS `widget_order`(
-      order_id INT unsigned PRIMARY KEY NOT NULL,
+      order_id CHAR(26) PRIMARY KEY NOT NULL,
       quantity INT unsigned NOT NULL,
       color VARCHAR(10) NOT NULL,
-      tyoe VARCHAR(20) NOT NULL,
+      widget_type VARCHAR(20) NOT NULL,
       date_needed INT unsigned NOT NULL,
       create_date INT unsigned NOT NULL
     )
--- index.php
+++ index.php
@@ -6,32 +6,50 @@
  * Time: 7:39 PM
  */
 ini_set('display_errors', 1);
-require 'Database.php';
+require 'Model.class.php';
 
+//filter user inputs
 function escapeData($data)
 {
-    return addslashes(strip_tags(trim($data)));
+    return strip_tags(trim($data));
 }
 
-$num1 = uniqid();
-$num2 = rand(100, 999);
-$num3 = time();
-
-$orderID = $num1 . $num2 . $num3;
-
-
+//Params for SQL
+$orderID = uniqid() . rand(100, 999) . time();
 $quantity = escapeData($_POST['quantity']);
 $color = escapeData($_POST['color']);
 $date = strtotime($_POST['date']);
 $type = escapeData($_POST['type']);
+$create = time();
 
-$db = new DB();
-$sql = "INSERT INTO `widget_order` VALUES ('$orderID', '$quantity', '$color', '$date', '$type', time())";
-$res = $db->exec($sql);
-
-if ($res) {
-    return 'Your order has been placed, Order ID is'.$orderID;
-} else {
-    print 'Please try again';
+//validation for null inputs
+if (empty($quantity) || empty($date)) {
+    print 'Some content is missing.';
+    die();
 }
+//validation for date of needed
+if(($date - time())<604800){
+    print 'The date format is either incorrect or not valid, please try again.';
+    die();
+}
+
+//save data to database
+$db = new Model();
+$res = $db->createData($orderID,$quantity,$color,$type,$date,$create);
+if ($res) {
+    print 'Your order has been placed, Order ID is '.$orderID.'.';
+    print '\n';
+} else {
+    print 'Something went wrong, please try again later!';
+}
+
+//$res = $db->readData();
+//while($row = $res->fetchArray(SQLITE3_ASSOC) ){
+//    echo "orderID = ". $row['order_id'] . "\n";
+//    echo "quantity = ". $row['quantity'] ."\n";
+//    echo "color = ". $row['color'] ."\n";
+//    echo "widget_type =  ".$row['widget_type'] ."\n\n";
+//    echo "date_needed =  ".$row['date_needed'] ."\n\n";
+//    echo "create_date =  ".($row['create_date'])."\n\n";
+//}
 
